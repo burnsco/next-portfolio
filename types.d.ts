91b4082a83112f159ef129ef1f68e37fc0b6ff47
Corey Burns <corey.burns@mailfence.com>
2021-01-15 14:13:25 -0500

    reverting back to old single page style...

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,19 @@
+type RefsRegister = {
+  [x: string]: RefObject<HTMLElement>
+}
+
+type Meta = {
+  [id: string]: unknown
+}
+
+type ScrollContextType = {
+  registerRef: ({ id: string, meta: unknown }) => RefObject<HTMLElement> | null
+  scrollTo: (section: string) => void
+  refs: RefsRegister
+  meta: Meta
+  selected: string
+}
+
 type MiniAppsType = {
   miniApps: {
     title: string
