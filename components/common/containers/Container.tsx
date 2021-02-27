744ab122ecd305bdfde53878ffae549f5a7d336f
Corey Burns <coreymburns@gmail.com>
2021-02-27 12:38:16 -0500

    getting ready for deploy

--- components/common/containers/Container.tsx
+++ components/common/containers/Container.tsx
@@ -1,25 +1,16 @@
 import type { BoxProps } from "@chakra-ui/react"
-import { Box, useColorMode } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 
-export const Container = (props: BoxProps) => {
-  const { colorMode } = useColorMode()
-
-  const bgColor = { light: "gray.50", dark: "gray.900" }
-  const color = { light: "black", dark: "white" }
-
-  return (
-    <Box
-      w="full"
-      pb="12"
-      pt="3"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
-      mx="auto"
-      maxW="75rem"
-      px={{ base: "2", md: "4" }}
-      {...props}
-    />
-  )
-}
+export const Container = (props: BoxProps) => (
+  <Box
+    w="full"
+    pb="12"
+    pt="3"
+    mx="auto"
+    maxW="75rem"
+    px={{ base: "2", md: "4" }}
+    {...props}
+  />
+)
 
 export default Container
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class Document extends NextDocument {
         <Head />
         <body>
           <Main />
-          <ColorModeScript initialColorMode="light" />
+          <ColorModeScript initialColorMode="dark" />
           <NextScript />
         </body>
       </Html>
