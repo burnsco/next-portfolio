030e1aa6204e3abe780b81c5914b428a5d936614
Corey <cburns@pop-os.localdomain>
2022-07-18 09:46:59 -0400

    updated deps, fixed some packages

--- components/common/icons/scroll-down-arrow.tsx
+++ components/common/icons/scroll-down-arrow.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion"
 const AnimatedScrollDownArrow = () => (
   <chakra.div
     display="flex"
-    align="center"
     id="scroll-down-div"
     pos="relative"
     userSelect="none"
